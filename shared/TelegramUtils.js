34af88e92e4976ed5a6b5a906e0e584fc3e03b52
lucas duval <duval.lucas@hotmail.fr>
2021-09-23 21:36:25 +0200

    add menu in bot chat

--- shared/TelegramUtils.js
+++ shared/TelegramUtils.js
@@ -5,7 +5,7 @@ const { Bot } = require("grammy");
 
 let bot;
 
-const init = () => {
+const init = async () => {
     if (typeof constVal.telegramBotToken === 'undefined' || constVal.telegramBotToken.length === 0){
         console.log(`You need to provide a bot token in ${constVal.envFile}, telegramBotToken`);
         return;
@@ -18,18 +18,22 @@ const init = () => {
         await utils.saveTelegramChatId();
         ctx.reply("Welcome! Up and running.")
     });
-    bot.command("getStats", async (ctx) => {
-        ctx.reply(`Preparing stats (it may take a long time)`);
-        //ctx.reply(`/!\\it may take a long time if you have a lot of token/!\\`);
-        let data = await statsUtils.getGlobalStats();
-        let text = statsUtils.formatGlobalStats(data);
-        ctx.reply(text);
-    });
-
+    bot.command("getStats", sendStats);
+    bot.command("getstats", sendStats);
+    await bot.api.setMyCommands([
+        { command: "getstats", description: "Display global stats" },
+    ]);
 // Start your bot
     console.log("Bot started");
     bot.start();
+}
 
+const sendStats = async (ctx) => {
+    ctx.reply(`Preparing stats (it may take a long time)`);
+    //ctx.reply(`/!\\it may take a long time if you have a lot of token/!\\`);
+    let data = await statsUtils.getGlobalStats();
+    let text = statsUtils.formatGlobalStats(data);
+    ctx.reply(text);
 }
 
 const sendMessage = (message) => {
